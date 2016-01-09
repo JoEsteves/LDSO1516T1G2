d1d48a73e216640745389d0bac4303fa4ea8427a
ricardomelocastro <ricardo.melo.castro@gmail.com>
2016-01-09 02:35:14 +0100

    bug fix index

--- CityFix/db/schema.rb
+++ CityFix/db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160105020338) do
 
-  create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.boolean  "public"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "statuses", force: :cascade do |t|
     t.string   "stype",      null: false
     t.datetime "created_at", null: false
